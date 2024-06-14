cbdfcbd831331cf943dea517713183fb3bd99e97
mschfh <37435502+mschfh@users.noreply.github.com>
2024-06-14 18:15:43 -0500

    Add zstd support (#10)

--- lambda/app.py
+++ lambda/app.py
@@ -1,14 +1,22 @@
 import subprocess
+import base64
+import zstandard as zstd
 
 CMD = ["/opt/webkit-content-filter-validator/webkit-content-filter-validator"]
 CWD = "/tmp"  # binary creates temp files, only /tmp is writeable to its UID
 SUCCESS = 0
 
+dctx = zstd.ZstdDecompressor()
 
 def handler(event, context):
     try:
+        if event.get('headers', {}).get('content-encoding') == 'application/zstd':
+            if event.get('isBase64Encoded', False):
+                body = dctx.decompress(base64.b64decode(event["body"])).decode('utf-8')
+        else:
+            body = event["body"]
         ps = subprocess.run(CMD, text=True, cwd=CWD, check=False, stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT, input=event["body"], timeout=60)
+                            stderr=subprocess.STDOUT, input=body, timeout=60)
         if ps.returncode == SUCCESS:
             return {
                 "statusCode": 200,
